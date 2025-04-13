185cc02ae51b74efc411b2fe9a1d1cc71652cb6c
Pablo Ruiz <pablo_ruiza@hotmail.com>
2025-04-13 10:53:08 -0500

    role and validationToken middlewares were created, these middlewares check the permissions

---
+++ src/middlewares/role.handler.js
@@ -0,0 +1,25 @@
+import jwt from "jsonwebtoken";
+
+const roleHandler = (req, res, next) =>{
+    const authHeader = req.headers.authorization;
+
+    if(!authHeader || !authHeader.startsWith('Bearer ')){
+        return res.status(401).json({ message: "Unauthorized: No token provided" });
+    }
+
+    const token = authHeader.split(' ')[1];
+
+    try{
+        const decoded = jwt.verify(token, process.env.JWT_KEY)
+        if(decoded.role === 1){
+            req.user = decoded
+            next()
+        }else{
+            return res.status(401).json({ message: "Unauthorized: You do not have the permissions to perform this action" });
+        }
+    }catch(error){
+        return res.status(401).json({ message: "Unauthorized: Invalid token" });
+    }
+}
+
+export default roleHandler
