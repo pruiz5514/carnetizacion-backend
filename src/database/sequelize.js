dc526b7bd1ee6f9a5feaf3c6f52853451cc40a14
Pablo Ruiz <pablo_ruiza@hotmail.com>
2025-04-15 23:56:41 -0500

    change configuration of sequelize file

--- src/database/sequelize.js
+++ src/database/sequelize.js
@@ -1,7 +1,14 @@
 import { Sequelize } from "sequelize";
 import 'dotenv/config';
 
-export const sequelize = new Sequelize (process.env.DB_DATABASE, process.env.DB_USER, process.env.DB_PASSWORD, {
-    host: 'localhost',
-    dialect: 'postgres'
-}) 
+export const sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres',
+    protocol: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false, 
+      }
+    }
+  });
+  
